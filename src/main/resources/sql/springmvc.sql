c8eed46f6822182857ab34ab8a45139e78ef2aa5
hu <hu@HUZHANGLIN218>
2018-06-13 22:34:21 +0800

    sql更新

--- src/main/resources/sql/springmvc.sql
+++ src/main/resources/sql/springmvc.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50549
 File Encoding         : 65001
 
-Date: 2018-06-13 07:54:11
+Date: 2018-06-13 22:33:08
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -31,6 +31,66 @@ CREATE TABLE `tb_course` (
 INSERT INTO `tb_course` VALUES ('1', '语文');
 INSERT INTO `tb_course` VALUES ('2', '数学');
 INSERT INTO `tb_course` VALUES ('3', '英语');
+
+-- ----------------------------
+-- Table structure for `tb_items`
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_items`;
+CREATE TABLE `tb_items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL COMMENT '商品名称',
+  `price` float(10,1) NOT NULL COMMENT '商品定价',
+  `detail` text COMMENT '商品描述',
+  `pic` varchar(64) DEFAULT NULL COMMENT '商品图片',
+  `createtime` datetime NOT NULL COMMENT '生产日期',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tb_items
+-- ----------------------------
+INSERT INTO `tb_items` VALUES ('1', '铅笔', '2.0', '一种用来书写以及绘画素描专用的笔类', 'pen.jpg', '2018-06-13 20:18:38');
+INSERT INTO `tb_items` VALUES ('2', '钢笔', '10.0', '钢笔是人们普遍使用的书写工具，发明于19世纪初。笔头由金属制成，书写起来圆滑而有弹性，相当流畅。在笔套口处或笔尖表面，均有明显的商标牌号、型号。还分为蘸水式钢笔和自来水式钢笔、墨囊钢笔。', 'gangbi.jpg', '2018-06-13 20:20:13');
+INSERT INTO `tb_items` VALUES ('3', '小刀', '2.0', '普通的铅笔刀一般是有三部分构成：刀片，刀鞘以及废料盒。', 'qianbidao.jpg', '2018-06-13 20:21:09');
+
+-- ----------------------------
+-- Table structure for `tb_orderdetail`
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_orderdetail`;
+CREATE TABLE `tb_orderdetail` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `orders_id` int(11) NOT NULL COMMENT '订单id',
+  `items_id` int(11) NOT NULL COMMENT '商品id',
+  `items_num` int(11) DEFAULT NULL COMMENT '商品购买数量',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tb_orderdetail
+-- ----------------------------
+INSERT INTO `tb_orderdetail` VALUES ('1', '1', '1', '1');
+INSERT INTO `tb_orderdetail` VALUES ('2', '1', '2', '9');
+INSERT INTO `tb_orderdetail` VALUES ('3', '2', '1', '5');
+INSERT INTO `tb_orderdetail` VALUES ('4', '2', '2', '5');
+
+-- ----------------------------
+-- Table structure for `tb_orders`
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_orders`;
+CREATE TABLE `tb_orders` (
+  `user_id` int(11) NOT NULL COMMENT '下单用户id',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `number` varchar(32) NOT NULL COMMENT '订单号',
+  `createtime` datetime NOT NULL COMMENT '创建订单时间',
+  `note` varchar(100) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tb_orders
+-- ----------------------------
+INSERT INTO `tb_orders` VALUES ('1', '1', '1', '2018-06-13 20:21:59', null);
+INSERT INTO `tb_orders` VALUES ('1', '2', '2', '2018-06-13 21:06:47', null);
 
 -- ----------------------------
 -- Table structure for `tb_student`
@@ -81,4 +141,4 @@ CREATE TABLE `tb_wx_user` (
 -- ----------------------------
 -- Records of tb_wx_user
 -- ----------------------------
-INSERT INTO `tb_wx_user` VALUES ('java', '123', '12', '123456');
+INSERT INTO `tb_wx_user` VALUES ('java', '123', '1', '123456');
