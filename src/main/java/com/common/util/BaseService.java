d6a79e81cb679e64049af90b20f0c372c7e18cdb
hu <hu@HUZHANGLIN218>
2018-06-21 23:14:41 +0800

    增加log

---
+++ src/main/java/com/common/util/BaseService.java
@@ -0,0 +1,8 @@
+package com.common.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class  BaseService {
+	 final Logger logger = LoggerFactory.getLogger(BaseController.class);
+}
---
+++ src/main/java/com/user/controller/OrderController.java
@@ -0,0 +1,7 @@
+package com.user.controller;
+
+import com.common.util.BaseController;
+
+public class OrderController extends BaseController {
+	
+}
